2eb49eb77dd8d4e39233ae302f924728f2da2741
Odialko <viktor1532@gmail.com>
2023-05-25 12:35:45 +0200

    firebase now working

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_deep_dive/firebase_options.dart';
 import 'package:flutter_deep_dive/src/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const App());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
