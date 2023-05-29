091fac1949b5c3b50b4ab880c1342784e4f13d3c
Odialko <viktor1532@gmail.com>
2023-05-29 15:22:21 +0200

    navigation done

--- lib/src/router/redirect.dart
+++ lib/src/router/redirect.dart
@@ -3,11 +3,9 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_deep_dive/src/providers/auth_provider.dart';
 import 'package:flutter_deep_dive/src/router/routes.dart';
-import 'package:flutter_deep_dive/src/router/routes_extension.dart';
-import 'package:flutter_deep_dive/src/ui/authentication/auth_store.dart';
+// import 'package:flutter_deep_dive/src/router/routes_extension.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-bool isAuth = false;
 
 String? authRedirect(
   BuildContext context,
@@ -18,43 +16,12 @@ String? authRedirect(
     'current location: ${state.location}',
     name: 'router check',
   );
-  final isLogInFlow = state.location.isInLoginFlow;
 
-  final isAuthNull = ref.read(authStateChangesProvider);
-  print('==========================isAuthNull>>>>> ${isAuthNull}');
-
-  ref.listen(authStateChangesProvider, <String>(previous, next) {
-    print('==============================next.value========> ${next.value}');
-    if (next.value != null) {
-      isAuth = true;
-      return Routes.home;
-    } else {
-      isAuth = false;
-      return Routes.login;
-    }
-  });
-
-
-
+  final isAuth = ref.read(authStateChangesProvider).value != null;
   final isSplash = state.location == Routes.splash;
+  // final isLoginFlow = state.location.isInLoginFlow;
 
   if (isSplash) return null;
 
-
-
-  print('===============isAuth:> $isAuth');
-
-
-
-  // print('===============isLogInFlow:> $isLogInFlow');
-  //
-  // if (isAuthNull && isLogInFlow) return null;
-  //
-  // print('===============(isAuthNull && isLogInFlow:> ${(isAuthNull && isLogInFlow)}');
-
-  // if (isLogInFlow) return isAuth ? null : Routes.home;
-
-  // if (isLogInFlow) return null;
-
-  return isAuth ? Routes.home : null;
+  return isAuth ? Routes.home : Routes.login;
 }
